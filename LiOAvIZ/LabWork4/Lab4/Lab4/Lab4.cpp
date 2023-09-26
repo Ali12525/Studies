4d7c08e77e8ffe7bd2ec7653b47c4ccb2733cf1c
Ali <alihuseyni523@gmail.com>
2023-09-26 20:37:58 +0300

    Lab 4 exercise 1,2,3

--- LiOAvIZ/LabWork4/Lab4/Lab4/Lab4.cpp
+++ LiOAvIZ/LabWork4/Lab4/Lab4/Lab4.cpp
@@ -3,36 +3,88 @@
 #include <stdlib.h>
 
 struct tnode* add_element(int x, tnode* tree);
+struct tnode* add_unequal_element(int x, tnode* tree);
 void print_tree(struct tnode* tree);
 void find(struct tnode* tree, int element);
+void total(struct tnode* tree, int element);
+
+int count = 0;
 
 int main()
 {
-    int start = 1;
-    int data = 0, element = 9;
+	int start = 1;
+	int data = 0, element = 9;
+	char ch;
+	int ext = 0;
+	struct tnode* root = NULL;
 
-    struct tnode* root = NULL;
-    printf("-1 - end\n");
-    while (start)
-    {
-        printf("Enter number: ");
-        scanf("%d", &data);
-        if (data == -1)
-        {
-            printf("end tree\n");
-            start = 0;
-        }
-        else
-            root = add_element(data, root);
-    }
+	do
+	{
+		system("cls");
+		printf("1. Add element\n2. Adding unequal elements\n3. View tree\n4. Find element\n5. Occurrence of the element\n");
+		printf("Select item: ");
+		scanf("%c", &ch);
+		system("cls");
+		if (ch != '\n')
+		{
+			scanf("%*[^\n]");
+			getchar();
+		}
 
-    print_tree(root);
-    find(root, element);
+		switch (ch)
+		{
+		case '1':
+			printf("-1 - end\n");
+			printf("Enter number: ");
+			scanf("%d", &data);
+			root = add_element(data, root);
+			break;
+		case '2':
+			printf("-1 - end\n");
+			printf("Enter number: ");
+			scanf("%d", &data);
+			root = add_unequal_element(data, root);
+			printf("\nPress any key to exit");
+			if (count > 0)
+			{
+				count = 0;
+				getchar();
+				getchar();
+			}
+			break;
+		case '3':
+			printf("Tree: ");
+			print_tree(root);
+			printf("\nPress any key to exit");
+			getchar();
+			break;
+		case '4':
+			printf("Enter element: ");
+			scanf("%d", &element);
+			find(root, element);
+			printf("Press any key to exit");
+			getchar();
+			getchar();
+			break;
+		case '5':
+			printf("Enter element: ");
+			scanf("%d", &element);
+			total(root, element);
+			printf("total: %d", count);
+			count = 0;
+			printf("\nPress any key to exit");
+			getchar();
+			getchar();
+			break;
+		default:
+			break;
+		}
+	} while (ext == 0);
 
 	return 0;
 }
 
-struct tnode 
+struct tnode
 {
 	int data;
 	struct tnode* left;
@@ -41,36 +93,70 @@ struct tnode
 
 struct tnode* add_element(int x, tnode* tree)
 {
-    if (tree == NULL) 
-    {
-        tree = (struct tnode*)malloc(sizeof(struct tnode));
-        tree->data = x;
-        tree->left = NULL;
-        tree->right = NULL;
-    }
-    else  if (x < tree->data)
-        tree->left = add_element(x, tree->left);
-    else
-        tree->right = add_element(x, tree->right);
-    return(tree);
+	if (tree == NULL)
+	{
+		tree = (struct tnode*)malloc(sizeof(struct tnode));
+		tree->data = x;
+		tree->left = NULL;
+		tree->right = NULL;
+	}
+	else  if (x < tree->data)
+		tree->left = add_element(x, tree->left);
+	else
+		tree->right = add_element(x, tree->right);
+	return(tree);
+}
+
+struct tnode* add_unequal_element(int x, tnode* tree)
+{
+	total(tree, x);
+	if (count > 0)
+	{
+		printf("Such an element already exists");
+		return(tree);
+	}
+
+	if (tree == NULL)
+	{
+		tree = (struct tnode*)malloc(sizeof(struct tnode));
+		tree->data = x;
+		tree->left = NULL;
+		tree->right = NULL;
+	}
+	else  if (x < tree->data)
+		tree->left = add_unequal_element(x, tree->left);
+	else
+		tree->right = add_unequal_element(x, tree->right);
+	return(tree);
 }
 
 void print_tree(struct tnode* tree)
 {
-    if (tree != NULL) {
-        print_tree(tree->left);
-        printf("%d \n", tree->data);
-        print_tree(tree->right);
-    }
+	if (tree != NULL) {
+		print_tree(tree->left);
+		printf("%d ", tree->data);
+		print_tree(tree->right);
+	}
 }
 
-void find(struct tnode* tree, int element) 
+void find(struct tnode* tree, int element)
 {
-    if (tree != NULL) 
-    {
-        find(tree->left, element);
-        if (tree->data == element)
-            printf("Element %d found\n", element);
-        find(tree->right, element);
-    }
+	if (tree != NULL)
+	{
+		find(tree->left, element);
+		if (tree->data == element)
+			printf("Element %d found\n", element);
+		find(tree->right, element);
+	}
+}
+
+void total(struct tnode* tree, int element)
+{
+	if (tree != NULL)
+	{
+		total(tree->left, element);
+		if (tree->data == element)
+			count++;
+		total(tree->right, element);
+	}
 }
