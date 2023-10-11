644ab54f91055cf9e66a58f1997e6c8a9d173a7f
Ali <alihuseyni523@gmail.com>
2023-10-12 00:01:50 +0300

    Lab work 6 exercise 1, 2

--- LiOAvIZ/LabWork6/Lab6/Lab6/Lab6.cpp
+++ LiOAvIZ/LabWork6/Lab6/Lab6/Lab6.cpp
@@ -5,18 +5,27 @@
 
 void fill_arr(int** Arr, int size);
 void print_arr(int** Arr, int size);
-int** vertexIndentification(int** Arr, int size, int u, int v);
+int** vertexIdentification2(int** Arr, int size, int u, int v);
+int** vertexSplitting(int** Arr, int size, int u, int v);
+int find_arr(int* Arr, int size, int element);
+int** vertexIdentification1(int** matrix, int size, int u, int v);
 struct node* createNode(int v);
 struct Graph* createGraph(int vertices);
 void addEdge(struct Graph* graph, int src, int dest);
 void printGraph(struct Graph* graph);
 struct Graph* transformGraph(int** Arr, int size);
+struct Graph* delVertexGraph(int** Arr, int size, int u);
+struct Graph* vertexIndentificationMatrix(struct Graph* graph, int** Arrey, int size, int u, int v);
+void printGraphDel(struct Graph* graph, int u, int k);
 
 int main(void)
 {
 	srand(time(NULL));
 	int sizeMatrix1, sizeMatrix2;
 	int** Arr, **Arrey;
+	char ch;
+	int ext = 0, extt = 0, priority = -1;
+	int u, v;
 
 	printf("Enter the size of the adjacency matrix 1: ");
 	scanf("%d", &sizeMatrix1);
@@ -48,9 +57,115 @@ int main(void)
 
 	printGraph(graph1);
 	printGraph(graph2);
+	printf("Press any key to exit");
+	getchar();
+	getchar();
 
-	Arr = vertexIndentification(Arr, sizeMatrix1, 1, 2);
-	print_arr(Arr, sizeMatrix1-1);
+	do
+	{
+		system("cls");
+		printf("1. Vertex indentification (Matrix)\n2. Vertex contraction (Matrix)\n3. Vertex splitting (Matrix)\n4. Vertex indentification (list)\n5. Vertex contraction (list)\n6. Vertex splitting (list)\n");
+		printf("Select item: ");
+		scanf("%c", &ch);
+		system("cls");
+		if (ch != '\n')
+		{
+			scanf("%*[^\n]");
+			getchar();
+		}
+
+		switch (ch)
+		{
+		case '1':
+			printf("Adjacency matrix 1\n");
+			print_arr(Arr, sizeMatrix1);
+			printf("\n\nAdjacency matrix 2\n");
+			print_arr(Arrey, sizeMatrix2);
+			printf("Enter vertex: \n");
+			printf("u = ");
+			scanf("%d", &u);
+			printf("v = ");
+			scanf("%d", &v);
+			Arr = vertexIdentification1(Arr, sizeMatrix1, u, v);
+			Arrey = vertexIdentification1(Arrey, sizeMatrix2, u, v);
+			printf("Result:\n");
+			print_arr(Arr, sizeMatrix1 - 1);
+			printf("\n");
+			print_arr(Arrey, sizeMatrix1 - 1);
+			printf("Press any key to exit");
+			getchar();
+			getchar();
+			break;
+		case '2':
+			printf("Adjacency matrix 1\n");
+			print_arr(Arr, sizeMatrix1);
+			printf("\n\nAdjacency matrix 2\n");
+			print_arr(Arrey, sizeMatrix2);
+			printf("Enter vertex: \n");
+			printf("u = ");
+			scanf("%d", &u);
+			printf("v = ");
+			scanf("%d", &v);
+			if (Arr[u][v] == 0)
+			{
+				printf("Error\n");
+				printf("Press any key to exit");
+				getchar();
+				getchar();
+				break;
+			}
+			Arr = vertexIdentification1(Arr, sizeMatrix1, u, v);
+			Arrey = vertexIdentification1(Arrey, sizeMatrix2, u, v);
+			printf("Result:\n");
+			print_arr(Arr, sizeMatrix1);
+			printf("\n");
+			print_arr(Arrey, sizeMatrix1);
+			printf("Press any key to exit");
+			getchar();
+			getchar();
+			break;
+		case '3':
+			printf("Adjacency matrix 1\n");
+			print_arr(Arr, sizeMatrix1);
+			printf("\n\nAdjacency matrix 2\n");
+			print_arr(Arrey, sizeMatrix2);
+			printf("Enter vertex: \n");
+			printf("u = ");
+			scanf("%d", &u);
+			printf("v = ");
+			scanf("%d", &v);
+			Arr = vertexSplitting(Arr, sizeMatrix1, u, v);
+			Arrey = vertexSplitting(Arrey, sizeMatrix2, u, v);
+			printf("Result:\n");
+			print_arr(Arr, sizeMatrix1 + 1);
+			printf("\n");
+			print_arr(Arrey, sizeMatrix1 + 1);
+			printf("Press any key to exit");
+			getchar();
+			getchar();
+			break;
+		case '4':
+			printf("Enter vertex: \n");
+			printf("u = ");
+			scanf("%d", &u);
+			printf("v = ");
+			scanf("%d", &v);
+			graph1 = vertexIndentificationMatrix(graph1, Arr, sizeMatrix1, u, v);
+			printGraphDel(graph1, u , v);
+			printf("\n");
+			getchar();
+			getchar();
+			break;
+		case '5':
+
+			break;
+		case '6':
+
+			break;
+		default:
+			break;
+		}
+	} while (ext == 0);
 
 	free(Arr);
 	free(Arrey);
@@ -82,7 +197,7 @@ void print_arr(int** Arr, int size)
 	}
 }
 
-int** vertexIndentification(int** Arr, int size, int u, int v)
+int** vertexIdentification2(int** Arr, int size, int u, int v)
 {
 	int** ArreyIndentification;
 	int* ArrU = (int*)malloc(size * sizeof(int));
@@ -101,8 +216,6 @@ int** vertexIndentification(int** Arr, int size, int u, int v)
 		
 	}
 
-	ArrZ[size - 2] = ArrZ[size-1];
-
 	for (int i = 0; i < size; i++)
 	{
 		Arr[u][i] = 0;
@@ -112,32 +225,32 @@ int** vertexIndentification(int** Arr, int size, int u, int v)
 		Arr[i][v] = 0;
 	}
 
+	ArreyIndentification = (int**)malloc(size * sizeof(int*));
+
+	for (int i = 0; i < size; ++i)
+	{
+		ArreyIndentification[i] = (int*)malloc(size * sizeof(int));
+	}
+
 	for (int i = 0; i < size; i++)
 	{
-		Arr[v][i] = Arr[size-1][i];
-		Arr[i][v] = Arr[i][size-1];
-	}
-
-	ArreyIndentification = (int**)malloc((size - 1) * sizeof(int*));
-
-	for (int i = 0; i < (size - 1); ++i)
-	{
-		ArreyIndentification[i] = (int*)malloc((size - 1) * sizeof(int));
-	}
-
-	for (int i = 0; i < (size - 1); i++)
-	{
-		for (int j = 0; j < (size - 1); j++)
+		for (int j = 0; j < size; j++)
 		{
 			ArreyIndentification[i][j] = Arr[i][j];
 		}
 	}
 
-	for (int i = 0; i < (size - 1); i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (u == i)
 		{
 			ArreyIndentification[u][i] = 0;
+			continue;
+		}
+		if (v == i)
+		{
+			ArreyIndentification[v][i] = 0;
+			ArreyIndentification[i][v] = 0;
 			continue;
 		}
 		ArreyIndentification[u][i] = ArrZ[i];
@@ -145,6 +258,220 @@ int** vertexIndentification(int** Arr, int size, int u, int v)
 	}
 
 	return ArreyIndentification;
+}
+
+int** vertexIdentification1(int** Arr, int size, int u, int v)
+{
+	int** ArreyIndentification;
+	ArreyIndentification = (int**)malloc((size - 1) * sizeof(int*));
+
+	for (int i = 0; i < size - 1; ++i)
+	{
+		ArreyIndentification[i] = (int*)malloc((size - 1) * sizeof(int));
+	}
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			ArreyIndentification[i][j] = 0;
+		}
+	}
+
+	if (v < u)
+	{
+		int t = u;
+		u = v;
+		v = t;
+	}
+
+	int di = 0, dj = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (i == u || i == v)
+			i++;
+		if (i == u || i == v)
+			i++;
+
+		if (i >= size)
+			break;
+
+		for (int j = 0; j < size; j++)
+		{
+			if (j == u || j == v)
+				j++;
+			if (j == u || j == v)
+				j++;
+
+			if (j >= size)
+				break;
+
+			ArreyIndentification[di][dj] = Arr[i][j];
+			dj++;
+		}
+
+		di++;
+		dj = 0;
+	}
+
+	int k = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (i == u || i == v)
+			i++;
+		if (i == u || i == v)
+			i++;
+		if (i >= size)
+			break;
+		if (Arr[v][i] == 1)
+		{
+			ArreyIndentification[size - 2][k] = 1;
+			ArreyIndentification[k][size - 2] = 1;
+		}
+		if (Arr[u][i] == 1)
+		{
+			ArreyIndentification[size - 2][k] = 1;
+			ArreyIndentification[k][size - 2] = 1;
+		}
+		k++;
+	}
+
+	return ArreyIndentification;
+}
+
+
+int** vertexSplitting(int** Arr, int size, int u, int v)
+{
+	int** ArreyIndentification;
+
+	ArreyIndentification = (int**)malloc((size + 1) * sizeof(int*));
+
+	for (int i = 0; i < size + 1; ++i)
+	{
+		ArreyIndentification[i] = (int*)malloc((size + 1) * sizeof(int));
+	}
+
+	for (int i = 0; i < size + 1; i++)
+	{
+		for (int j = 0; j < size + 1; j++)
+		{
+			if (i == size || j == size) {
+				ArreyIndentification[i][j] = 0;
+				continue;
+			}
+			ArreyIndentification[i][j] = Arr[i][j];
+		}
+	}
+
+	ArreyIndentification[u][v] = 0;
+	ArreyIndentification[v][u] = 0;
+
+	if (Arr[u][v] == 1)
+	{
+		ArreyIndentification[u][size] = 1;
+		ArreyIndentification[size][u] = 1;
+		ArreyIndentification[v][size] = 1;
+		ArreyIndentification[size][v] = 1;
+	}
+
+	return ArreyIndentification;
+}
+
+struct Graph* vertexIndentificationMatrix(struct Graph* graph, int** Arrey, int size, int u, int v)
+{
+	int count = 0, count2 = 0;
+	struct node* temp = graph->adjLists[v];
+	int* Arr = (int*)malloc(count * sizeof(int));
+	int* Arr2 = (int*)malloc(count2 * sizeof(int));
+
+	while (temp)
+	{
+		count++;
+		Arr = (int*)realloc(Arr, count * sizeof(int));
+		Arr[count - 1] = temp->vertex;
+
+		temp = temp->next;
+	}
+
+	temp = graph->adjLists[u];
+
+	while (temp)
+	{
+		if (temp->vertex == v)
+		{
+			temp = temp->next;
+			continue;
+		}
+		if (find_arr(Arr, count, temp -> vertex) == 1)
+		{
+			count2++;
+			Arr2 = (int*)realloc(Arr2, count2 * sizeof(int));
+			Arr2[count2 - 1] = temp->vertex;
+		}
+		temp = temp->next;
+	}
+
+	graph = delVertexGraph(Arrey, size, u);
+	printGraph(graph);
+
+	for (int i = 0; i < count2; i++)
+	{
+		addEdge(graph, v, Arr2[i]);
+	}
+
+	struct Graph* graph2 = createGraph(size - 1);
+	int k = 0;
+
+	for (int i = 0; i < graph2->numVertices + 1; i++)
+	{
+		if (i == u)
+		{
+			continue;
+		}
+		struct node* temp = graph->adjLists[i];
+		graph2->adjLists[k] = temp;
+		k++;
+	}
+
+	printGraph(graph);
+
+	return graph2;
+}
+
+struct Graph* delVertexGraph(int** Arr, int size, int u)
+{
+	struct Graph* graph = createGraph(size);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (i == u)
+			continue;
+		for (int j = i; j < size; j++)
+		{
+			if (j == u)
+				continue;
+			if (Arr[i][j] == 1)
+			{
+				addEdge(graph, i, j);
+			}
+		}
+	}
+
+	return graph;
+}
+
+int find_arr(int* Arr, int size, int element)
+{
+	for (int j = 0; j < size; j++)
+	{
+		if (element == Arr[j])
+		{
+			return 0;
+		}
+	}	
+
+	return 1;
 }
 
 void fill_arr(int** Arr, int size)
@@ -211,8 +538,6 @@ struct Graph* transformGraph(int** Arr, int size)
 	return graph;
 }
 
-
-
 void addEdge(struct Graph* graph, int src, int dest)
 {
 	struct node* newNode = createNode(dest);
@@ -232,6 +557,36 @@ void printGraph(struct Graph* graph)
 	{
 		struct node* temp = graph->adjLists[v];
 		printf("\n|%d|", v);
+		while (temp)
+		{
+			printf(" -> %d", temp->vertex);
+			temp = temp->next;
+		}
+	}
+	printf("\n");
+}
+
+void printGraphDel(struct Graph* graph, int u, int k)
+{
+	printf("\nAdjancecy list");
+
+	for (int v = 0; v < graph->numVertices; v++)
+	{
+		struct node* temp = graph->adjLists[v];
+		
+		if (v == k - 1)
+		{
+			printf("\n|%d%d|", u, k);
+		}
+		else if (v >= u)
+		{
+			printf("\n|%d|", v + 1);
+		}
+		else
+		{
+			printf("\n|%d|", v);
+		}
+	
 		while (temp)
 		{
 			printf(" -> %d", temp->vertex);
