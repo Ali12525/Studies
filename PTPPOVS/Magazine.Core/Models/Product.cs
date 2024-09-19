f94dc5eafb9824a8193c3a7b13e9e7a3adbbdc4a
Ali <alihuseyni523@gmail.com>
2024-09-19 22:20:58 +0300

    Lab1 4, 6-7

---
+++ PTPPOVS/Magazine.Core/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Core.Models
+{
+    internal class Product
+    {
+        private int id;
+        private string definition;
+        private string name;
+        private int price;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Definition
+        {
+            get { return definition; }
+            set { definition = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
