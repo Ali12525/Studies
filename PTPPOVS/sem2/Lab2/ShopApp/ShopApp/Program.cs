0444dc2455df8cbe03e0123a6390637fbbabd494
Ali <alihuseyni523@gmail.com>
2025-03-02 22:41:00 +0300

    PTPPOVS lab2

---
+++ PTPPOVS/sem2/Lab2/ShopApp/ShopApp/Program.cs
@@ -0,0 +1,32 @@
+using ShopApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var basePath = AppContext.BaseDirectory;
+    var xmlPath = Path.Combine(basePath, "ShopApp.xml");
+    options.IncludeXmlComments(xmlPath);
+});
+
+// Регистрация IProductService и его реализации
+builder.Services.AddSingleton<IProductService, ProductService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
