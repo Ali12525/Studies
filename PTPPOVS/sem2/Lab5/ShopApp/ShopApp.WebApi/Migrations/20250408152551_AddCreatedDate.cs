e847ec9efc437c1634c5571dd7a97e35ef3d9a81
Ali <alihuseyni523@gmail.com>
2025-04-09 11:56:21 +0300

    PTPPOVS lab5 upd

---
+++ PTPPOVS/sem2/Lab5/ShopApp/ShopApp.WebApi/Migrations/20250408152551_AddCreatedDate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApp.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Id",
+                table: "Products",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Id",
+                table: "Products",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ PTPPOVS/sem2/Lab5/ShopApp/ShopApp.WebApi/Migrations/20250408155016_AddCreatedDate3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApp.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedDate3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
