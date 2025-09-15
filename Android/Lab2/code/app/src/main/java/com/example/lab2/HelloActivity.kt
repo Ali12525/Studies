8053bd9a53beb86532ab9a1f0857037a72792941
Ali <alihuseyni523@gmail.com>
2025-09-15 18:23:49 +0300

    Android Lab2 upd

--- Android/Lab2/code/app/src/main/java/com/example/lab2/HelloActivity.kt
+++ Android/Lab2/code/app/src/main/java/com/example/lab2/HelloActivity.kt
@@ -2,16 +2,28 @@ package com.example.lab2
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 
 class HelloActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_helloact)
 
-        val button0 = findViewById<Button>(R.id.button0)
-        button0.setOnClickListener {
-            button0.text = "НАЖАТО!"
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            button1.text = "НАЖАТО!"
         }
+
+        val myStringArray = ArrayList<String>()
+        myStringArray.add("Element_0")
+        myStringArray.add("Element_1")
+        myStringArray.add("Element_2")
+        myStringArray.add("Element_3")
+        val textAdapter: ArrayAdapter<String> = ArrayAdapter(this, R.layout.item,
+            R.id.itemContent, myStringArray)
+        val textList = findViewById<ListView>(R.id.textList)
+        textList.adapter = textAdapter
     }
 }
