31abd16ff4bd23eeb4a8ccc943b5693b0f969834
Ali <alihuseyni523@gmail.com>
2026-03-22 14:20:32 +0300

    add Lab3

---
+++ DesignPatterns/Lab3/NoPattern/Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,71 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+
+class UniversalCard {
+private:
+    bool hasPassport_ = false;
+    bool hasInsurance_ = false;
+    bool hasBankCard_ = false;
+
+    std::string passportSeries_;
+    std::string passportNumber_;
+    std::string insuranceNumber_;
+    std::string bankCardNumber_;
+
+public:
+    void addPassport(const std::string& series, const std::string& number) {
+        hasPassport_ = true;
+        passportSeries_ = series;
+        passportNumber_ = number;
+    }
+
+    void addInsurance(const std::string& number) {
+        hasInsurance_ = true;
+        insuranceNumber_ = number;
+    }
+
+    void addBankCard(const std::string& number) {
+        hasBankCard_ = true;
+        bankCardNumber_ = number;
+    }
+
+    std::string getDescription() const {
+        std::string result = "Универсальная электронная карта";
+        if (hasPassport_) {
+            result += "\nПаспорт: серия " + passportSeries_ + " номер " + passportNumber_;
+        }
+        if (hasInsurance_) {
+            result += "\nСтраховой полис: № " + insuranceNumber_;
+        }
+        if (hasBankCard_) {
+            result += "\nБанковская карта: номер " + bankCardNumber_;
+        }
+        return result;
+    }
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    UniversalCard basicCard;
+    std::cout << "Базовая карта:\n" << basicCard.getDescription() << "\n\n";
+
+    UniversalCard cardWithPassport;
+    cardWithPassport.addPassport("", "");
+    std::cout << "С паспортом:\n" << cardWithPassport.getDescription() << "\n\n";
+
+    UniversalCard cardWithPassportAndInsurance;
+    cardWithPassportAndInsurance.addPassport("", "");
+    cardWithPassportAndInsurance.addInsurance("");
+    std::cout << "С паспортом и страховкой:\n" << cardWithPassportAndInsurance.getDescription() << "\n\n";
+
+    UniversalCard fullCard;
+    fullCard.addPassport("", "");
+    fullCard.addInsurance("");
+    fullCard.addBankCard("");
+    std::cout << "Полная карта (паспорт + страховка + банк):\n" << fullCard.getDescription() << "\n";
+
+    return 0;
+}
---
+++ DesignPatterns/Lab3/Pattern/Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,133 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <memory>
+#include <string>
+
+class Card {
+public:
+    virtual ~Card() = default;
+    virtual std::string getDescription() const = 0;
+    virtual std::string getPassport() const = 0;
+    virtual std::string getInsurance() const = 0;
+    virtual std::string getBankCard() const = 0;
+};
+
+class BasicCard : public Card {
+public:
+    std::string getDescription() const override {
+        return "Универсальная электронная карта";
+    }
+
+    std::string getPassport() const override {
+        return "";
+    }
+
+    std::string getInsurance() const override {
+        return "";
+    }
+
+    std::string getBankCard() const override {
+        return "";
+    }
+};
+
+class CardDecorator : public Card {
+protected:
+    std::unique_ptr<Card> card_;
+public:
+    CardDecorator(std::unique_ptr<Card> card) : card_(std::move(card)) {}
+};
+
+class PassportDecorator : public CardDecorator {
+    std::string series_;
+    std::string number_;
+public:
+    PassportDecorator(std::unique_ptr<Card> card,
+        const std::string& series,
+        const std::string& number)
+        : CardDecorator(std::move(card)), series_(series), number_(number) {
+    }
+
+    std::string getDescription() const override {
+        return card_->getDescription() + "\nПаспорт: серия " + series_ + " номер " + number_;
+    }
+
+    std::string getPassport() const override {
+        return card_->getPassport() + "\nПаспорт: серия " + series_ + " номер " + number_;
+    }
+
+    std::string getInsurance() const override {
+        return card_->getInsurance() + "";
+    }
+
+    std::string getBankCard() const override {
+        return card_->getBankCard() + "";
+    }
+};
+
+class InsuranceDecorator : public CardDecorator {
+    std::string policyNumber_;
+public:
+    InsuranceDecorator(std::unique_ptr<Card> card, const std::string& policyNumber)
+        : CardDecorator(std::move(card)), policyNumber_(policyNumber) {
+    }
+
+    std::string getDescription() const override {
+        return card_->getDescription() + "\nСтраховой полис: № " + policyNumber_;
+    }
+
+    std::string getPassport() const override {
+        return card_->getPassport() + "";
+    }
+
+    std::string getInsurance() const override {
+        return card_->getInsurance() + "\nСтраховой полис: № " + policyNumber_;
+    }
+
+    std::string getBankCard() const override {
+        return card_->getBankCard() + "";
+    }
+};
+
+class BankCardDecorator : public CardDecorator {
+    std::string cardNumber_;
+public:
+    BankCardDecorator(std::unique_ptr<Card> card, const std::string& cardNumber)
+        : CardDecorator(std::move(card)), cardNumber_(cardNumber) {
+    }
+
+    std::string getDescription() const override {
+        return card_->getDescription() + "\nБанковская карта: номер " + cardNumber_;
+    }
+
+    std::string getPassport() const override {
+        return card_->getPassport() + "";
+    }
+
+    std::string getInsurance() const override {
+        return card_->getInsurance() + "";
+    }
+
+    std::string getBankCard() const override {
+        return card_->getBankCard() + "\nБанковская карта: номер " + cardNumber_;
+    }
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    auto basic = std::make_unique<BasicCard>();
+    std::cout << "Базовая карта:\n" << basic->getDescription() << "\n\n";
+
+    auto withPassport = std::make_unique<PassportDecorator>(std::move(basic), "", "");
+    std::cout << "С паспортом:\n" << withPassport->getPassport() << "\n\n";
+
+    auto withPassportAndInsurance = std::make_unique<InsuranceDecorator>(std::move(withPassport), "");
+    std::cout << "С паспортом и страховкой:\n" << withPassportAndInsurance->getInsurance() << "\n\n";
+
+    auto fullCard = std::make_unique<BankCardDecorator>(std::move(withPassportAndInsurance), "");
+    std::cout << "Полная карта (паспорт + страховка + банк):\n" << fullCard->getBankCard() << "\n";
+
+    return 0;
+}
